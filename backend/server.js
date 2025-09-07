886f65b2fc32647df0e8f3c0e453ac84a4db7403
Gaurav Kumar Jaiswal <gauravjaiswal843@gmail.com>
2025-09-07 13:46:17 +0530

    Added MONGO URI

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(cookieParser());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/ai_project', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
